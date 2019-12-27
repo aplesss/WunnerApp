3481587e3378c749800b2003a242bf2576797d75
BuildTools <unconfigured@null.spigotmc.org>
2019-12-27 23:36:47 +0700

    initial commit

---
+++ app/src/main/java/com/production/wunner/Submit.java
@@ -0,0 +1,14 @@
+package com.production.wunner;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Submit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit);
+    }
+}
